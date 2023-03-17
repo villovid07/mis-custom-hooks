cb2a427cbef0adc01684888453875e51c1876e93
villovid07 <villovid07@gmail.com>
2023-03-17 16:13:55 -0500

    Primer Commit

---
+++ useTodos.js
@@ -0,0 +1,58 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+const initialState = [
+  /*{
+        id: new Date().getTime(),
+        description: 'Recolectar piedra del alma',
+        done: false,
+    },*/
+];
+
+export const useTodos = () => {
+  const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos) || []);
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add todo",
+      payload: todo,
+    };
+
+    dispatchTodo(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const action = {
+      type: "[TODO] Remove todo",
+      payload: id,
+    };
+
+    dispatchTodo(action);
+  };
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle todo",
+      payload: id,
+    };
+
+    dispatchTodo(action);
+  };
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+    handleDeleteTodo,
+    handleNewTodo,
+    handleToggleTodo,
+  };
+};
